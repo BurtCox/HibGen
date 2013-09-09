228af358e250b4c741b109c8b5a7c36fc2d3272e
e41887 <e41887@SY4-3N9FDQ1.LUV.AD.SWACORP.com>
2013-09-09 11:42:42 -0500

    Additional changes

--- src/com/hibgen/HibGen.java
+++ src/com/hibgen/HibGen.java
@@ -1,13 +1,24 @@
 package com.hibgen;
 
+import javax.imageio.spi.ServiceRegistry;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 public class HibGen {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		Configuration configuration = new Configuration();
+		configuration.configure("../../../properties/hibernate.cfg.xml");
+		ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder().applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistryBuilder.buildServiceRegistry());
+		sessionFactory.getCurrentSession();
 	}
 
 }
